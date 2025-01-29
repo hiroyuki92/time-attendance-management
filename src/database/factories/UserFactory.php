43a53608242ee13e9adeaf7b337f378360e073e0
yukiko <yukiko@magnolia.co.jp>
2025-01-29 13:48:05 +0900

    UserFactory作成

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -14,11 +14,13 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $faker = \Faker\Factory::create('ja_JP');
         return [
-            'name' => $this->faker->name(),
+            'name' => $faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'role' => 'user',
             'remember_token' => Str::random(10),
         ];
     }
@@ -33,6 +35,23 @@ class UserFactory extends Factory
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
+            ];
+        });
+    }
+
+    /**
+     * Define an admin state.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function admin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'role' => 'admin', // 管理者ユーザー
+                'email' => 'admin@example.com',
+                'name' => '管理者ユーザー',
+                'password' => bcrypt('password'),
             ];
         });
     }
