63fc914fc7de724b738221c52aaa56a76cbb7fb8
yukiko <yukiko@magnolia.co.jp>
2025-02-22 23:53:42 +0900

    wipテスト：勤怠詳細情報修正機能（一般ユーザー）

--- src/tests/Feature/UserAttendanceDetailsCorrectionTest.php
+++ src/tests/Feature/UserAttendanceDetailsCorrectionTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 use App\Models\User;
 use App\Models\Attendance;
 use App\Models\BreakTime;
+use App\Models\AttendanceModification;
 use Carbon\Carbon;
 use Symfony\Component\DomCrawler\Crawler;
 
@@ -16,10 +17,12 @@ class UserAttendanceDetailsCorrectionTest extends TestCase
     use RefreshDatabase;
 
     protected $user;
+    protected $admin;
 
     protected function setUp(): void
     {
         parent::setUp();
+        $this->admin = User::factory()->admin()->create();
         $this->user = User::factory()->create();
     }
 
@@ -191,6 +194,8 @@ class UserAttendanceDetailsCorrectionTest extends TestCase
 
         $updateData = [
             'attendance_id' => $attendance->id,
+            'requested_year' => now()->format('Y') . '年',
+            'requested_date' => now()->format('n月j日'),
             'requested_work_date' => now()->toDateString(),
             'requested_clock_in' => '09:00',
             'requested_clock_out' => '18:00',
@@ -205,6 +210,75 @@ class UserAttendanceDetailsCorrectionTest extends TestCase
         $response->assertSessionHasErrors([
             'reason' => '備考を記入してください。'
         ]);
+
+        Carbon::setTestNow();
+    }
+
+    /**
+     * @test
+     * 修正申請処理が実行されるかテスト
+     */
+    public function execute_mod_request()
+    {
+        $now = Carbon::create(2025, 2, 14, 9, 0);
+        Carbon::setTestNow($now);
+
+        $attendance = $this->user->attendances()->create([
+            'work_date' => now()->toDateString(),
+            'clock_in' => Carbon::create(2025, 2, 14, 9, 0),
+            'clock_out' => Carbon::create(2025, 2, 14, 18, 0),
+            'status' => 'left',
+        ]);
+
+        $response = $this->actingAs($this->user)
+            ->get("/attendance/{$attendance->id}");
+
+        $response->assertStatus(200);
+
+        $updateData = [
+            'attendance_id' => $attendance->id,
+            'requested_year' => now()->format('Y') . '年',
+            'requested_date' => now()->format('n月j日'),
+            'requested_work_date' => now()->toDateString(),
+            'requested_clock_in' => '10:00',
+            'requested_clock_out' => '18:00',
+            'reason' => '遅刻',
+            'status' => 'pending',
+        ];
+        $response = $this->actingAs($this->user)
+        ->post('/attendance/mod-request', $updateData);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseHas('attendance_mod_requests', [
+            'attendance_id' => $attendance->id,
+            'requested_clock_in' => Carbon::create(2025, 2, 14, 10, 0),
+            'requested_clock_out' => Carbon::create(2025, 2, 14, 18, 0),
+            'reason' => '遅刻',
+            'status' => 'pending',
+        ]);
+
+        $this->actingAs($this->admin,'admin');
+
+        $adminResponse = $this->get('/admin/stamp-correction/list');
+        $adminResponse->assertStatus(200);
+
+        $adminHtml = $adminResponse->getContent();
+        $crawler = new Crawler($adminHtml);
+
+
+        $this->assertStringContainsString('遅刻', $crawler->filter('td:nth-child(4)')->text());
+
+        $modRequest = AttendanceModification::where('attendance_id', $attendance->id)->first();
+        $approveResponse = $this->get("/admin/stamp-correction/approve/{$modRequest->id}");
+        $approveResponse->assertStatus(200);
+
+        $approveHtml = $approveResponse->getContent();
+        $approveCrawler = new Crawler($approveHtml);
+
+        $this->assertStringContainsString('遅刻', $approveCrawler->filter('.form-group_content-detail')->eq(4)->text());
+        $this->assertStringContainsString('10:00', $approveCrawler->filter('.form-group_content-detail')->eq(2)->text());
+        $this->assertStringContainsString('18:00', $approveCrawler->filter('.form-group_content-detail')->eq(3)->text());
 
         Carbon::setTestNow();
     }
